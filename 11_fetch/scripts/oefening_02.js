fe4aebfa80159902cc448a6f4587fc8da1b7bd23
ErayOzcanPXL <eray.ozcan@student.pxl.be>
2020-03-22 17:01:16 +0100

    11_fetch/scripts/oefening_02.js: made it possible to search friends

--- 11_fetch/scripts/oefening_02.js
+++ 11_fetch/scripts/oefening_02.js
@@ -1,6 +1,26 @@
 'use strict';
 
-// functions
+// ************************* functions *************************
+// ------------------------- private -------------------------
+// fetch
+function _checkResponse(response) {
+    if (response.ok) {
+        return response.json();
+    } else {
+        throw new Error(`Response error: ${response.status}`);
+    }
+}
+
+function _handleError(error, output) {
+    if (output !== undefined) {
+        output.appendChild(document.createTextNode(error.message));
+        _errorOutput = undefined;
+    } else {
+        alert(error.message);
+    }
+}
+
+// ------------------------- public -------------------------
 function loadScript() {
     fillSelect();
     document.getElementById("button_search_friends").addEventListener("click", searchFriends);
@@ -22,28 +42,61 @@ function fillSelect() {
 function searchFriends() {
     let output = document.getElementById("div_output");
     let select = document.getElementById("select_person");
-    let id = select.options[select.selectedIndex].value;
+    let personId = select.options[select.selectedIndex].value;
+    let personName = select.options[select.selectedIndex].textContent;
+    let friendsId, urlFilter;
+    let friendsNames = [];
 
-    fetch(_url + id)
+    output.innerHTML = "";
+    fetch(_url + personId)
         .then((response) => {
-            if (response.status === 200) {
-                return response.json();
-            } else {
-                throw new Error(String(response.status));
-            }
+            return _checkResponse(response);
         })
         .then((person) => {
+            /*
+            * opgave zegt: vriend van sofie is tim en geert --> false! geert is geen vriend van sofie
+            * dus ik heb oefening.json aangepast: sofie --> "friends": [2, 3]
+            * => logischer, mits het initieel al gaat om een array van vrienden
+            *
+            * volgens de normale opgave zou ik de vriend van sofie, en "de vriend van de vriend van sofie" moeten nemen
+            * == onlogisch want sofie heeft maar 1 vriend waarvan id = 2 (= tim)
+            * */
 
+            friendsId = person.friends;
+
+            if (friendsId.length !== 0) {
+                if (friendsId.length > 1) {
+                    urlFilter = `?id=${friendsId.join("&id=")}`;
+                } else {
+                    urlFilter = String(friendsId[0]);
+                }
+
+                return fetch(_url + urlFilter)
+            }
+
+            _errorOutput = output;
+            throw new Error(`${personName} heeft geen vrienden`);
         })
-        .catch((error) => handleError(error));
+        .then((response) => {
+            return _checkResponse(response);
+        })
+        .then((personFriends) => {
+            if (personFriends instanceof Array) {
+                for (let row in personFriends) {
+                    friendsNames.push(personFriends[row].name);
+                }
+            } else {
+                friendsNames.push(personFriends.name);
+            }
+
+            output.appendChild(document.createTextNode(`${personName} heeft vriend(en) ${friendsNames.join(", ")}`));
+        })
+        .catch((error) => _handleError(error, _errorOutput));
 }
 
-function handleError(error) {
-    alert(`Response error: ${error}`);
-}
-
-// members
+// ************************* private members *************************
 let _url = 'http://localhost:3000/persons/';
+let _errorOutput;
 
-// main
+// ************************* main *************************
 window.addEventListener("load", loadScript);
